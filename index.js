0ab59e761f417e9eb9fdc95925730b2a8fe64d32
carlos gomez <cgomez374@yahoo.com>
2021-03-30 19:54:21 -0700

    finished section 3, 4, and footer

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-//slider
-const persons = Array.from(document.querySelectorAll('.person'));
+const persons = document.querySelectorAll('.person');
+const slideContainer = document.querySelector('.testomonials');
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
 
-const slide = () => {
-    let x = 620;
-    let Pos1 = persons[0].getBoundingClientRect().x;
-    for(let i = 0; i < persons.length; i++){
-        persons[i].style.left = `${persons[i].getBoundingClientRect().x + x}px`;
+function handleClick(direction){
+    if(direction > 0){
+        persons.forEach(person => {
+            person.style.transform = `translate(-${350}px)`;
+        });
     }
-    
+    else if(direction < 0) {
+        persons.forEach(person => {
+            person.style.transform = `translate(${350}px)`;
+        });
+    }
 }
 
+next.addEventListener('click', () => handleClick(1));
+prev.addEventListener('click', () => handleClick(-1));
 
-// setInterval(() => {
-//     slide();
-// }, 1000)
 
-// slide();
+
+
+
